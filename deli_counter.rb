45490c331637fd1f73743d5bcaf83ace0bf412fd
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 04:10:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,16 @@
 katz_deli = []
 
 def line
-
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    
+  end
 end
 
-def take_a_number
+def take_a_number queue, person
+  queue.push person
+  puts "#{queue.size}"
 
 end
 
