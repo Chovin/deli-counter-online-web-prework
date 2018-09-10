c328ca955eea8c82e7a8650baa63ff42355d2e08
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 04:19:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ def now_serving queue
   if queue.size == 0
     puts "There is nobody waiting to be served!"
   else
-    "Currently serving #{queue.pop}."
+    "Currently serving #{queue.shift}."
   end
 end
