435a7fb23843af45229e3a3110eba80e984adee6
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 04:16:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line queue
   if queue.size == 0
     puts "The line is currently empty."
   else
-
+    puts "The line is currently: #{queue.map.with_index(1){|name, i| "#{i}. #{name}"}.join(" ")}"
   end
 end
 
